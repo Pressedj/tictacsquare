7f57880d53ea00896686a2a8e3087a1f0cf8180a
Juniper <jpress.box@gmail.com>
2022-09-22 22:48:45 -0400

    Completed main todos, few edgecases left but publishable

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-import { Howl, Howler } from 'howler';
-document.querySelector('#reset').addEventListener('click', clear)
+
 const tiles = document.querySelectorAll('.tile')
+const resetButton = document.querySelectorAll('.reset')
 const colors1 = document.querySelectorAll('.colors1')
 const colors2 = document.querySelectorAll('.colors2')
+const notifBoxStyle = document.querySelector('.centered').style
 const winTrios = ['.row1', '.row2', '.row3',
                   '.col1', '.col2', '.col3',
                   '.diag1', '.diag2']
 let player = true
 let colorToggle
 let turns = 0
+const tileSound = new Howl({
+    src: ['assets/sounds/tileplace.wav']
+});
+const winSound = new Howl({
+    src: ['assets/sounds/winSound.mp3']
+});
+const tieSound = new Howl({
+    src: ['assets/sounds/tieSound.mp3']
+});
+const resetSound = new Howl({
+    src: ['assets/sounds/reset.mp3']
+});
+alert('Pick a color?')
 
 //Adda check to make sure player1Color !== player2Color
 //player acts as a boolean vlalue to switch between the two players
@@ -49,7 +63,6 @@ function endCheck(){
 colors1.forEach(color1 => {
     console.log(color1)
     color1.addEventListener('click', function playerColorSelect() {
-        document.querySelector('h1').innerHTML = 'selecting color'
         //console.log(`Color is ${color1.style.background} nothing`, getComputedStyle(color1).backgroundColor)
         player1Color = getComputedStyle(color1).backgroundColor
         document.querySelector('.name1').style.background = player1Color
@@ -62,7 +75,6 @@ colors1.forEach(color1 => {
 colors2.forEach(color2 => {
     console.log(color2)
     color2.addEventListener('click', function playerColorSelect() {
-        document.querySelector('h1').innerHTML = 'selecting color'
         //console.log(`Color is ${color1.style.background} nothing`, getComputedStyle(color1).backgroundColor)
         player2Color = getComputedStyle(color2).backgroundColor
         //console.log(`Player1 Color is ${player1Color}`)
@@ -73,25 +85,22 @@ colors2.forEach(color2 => {
     
 })
 
-function playerColorChoice() {
-
-
-
-    //Make a button to change the gradient?
-    //
-
-}
 
 function pNotification(str) {
+    notifBoxStyle.display = 'flex'
+    document.querySelector('h2').innerHTML = str
+    str.toLowerCase() === 'its a tie' ? tieSound.play() : winSound.play()
     console.log(str)
-    setTimeout(clear(), 10000)
+      
 }
 
 //add event listener to all elements with class tile
 
 
 function clear() {
-    document.querySelector('h1').innerHTML = 'Board Reset, Waiting for Players..'
+    resetSound.play()
+    notifBoxStyle.display = 'none'
+    document.querySelector('#result').innerHTML = 'Board Reset, Waiting for Players..'
     showColorhoice()
     tiles.forEach(tile => tile.style.background = '#F0FFFF')
     turns = 0
@@ -112,16 +121,16 @@ function checkPlayerColors() {
 tiles.forEach(tile => {
     
     tile.addEventListener('click', function playerChoiceResponse() {
-        document.querySelector('h1').innerHTML = 'A match has begun!'
         //player1 equals true
         player === true ? colorToggle = player1Color : colorToggle = player2Color
         tile.style.background = colorToggle
+        tileSound.play()
         turns += 1
         endCheck()
-
         //function to change to next player color
         player === false ? player = true : player = false
         checkPlayerColors()
         
     })
 })
+resetButton.forEach(button => button.addEventListener('click', clear))
